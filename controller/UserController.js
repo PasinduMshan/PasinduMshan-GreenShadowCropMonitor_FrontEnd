7383ce6e950a2df038a277f42fcf65e40823a828
PasinduMshan <madushan01722@gmail.com>
2024-12-06 12:12:17 +0530

    create delete user parts

--- controller/UserController.js
+++ controller/UserController.js
@@ -211,3 +211,49 @@ function searchUser() {
         }
     });
 }
+
+// -----------------------------------Delete User-------------------------
+$("#UserTableBody").on('click', '.user-delete-btn', function () {
+    var userEmail = $(this).closest('tr').data('user-email');
+    console.log("Attempting to delete user with email:", userEmail);
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to undo this action!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'No, cancel!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                method: "DELETE",
+                url: baseUrl + `user/${userEmail}`,
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                },
+                success: function (response) {
+                    console.log("User deleted successfully:", response);
+                    $(`tr[data-user-email='${userEmail}']`).remove();
+                    Swal.fire(
+                        'Deleted!',
+                        'The user has been deleted successfully.',
+                        'success'
+                    );
+                },
+                error: function (error) {
+                    console.error("Error deleting user:", error);
+                    Swal.fire(
+                        'Error!',
+                        'There was an issue deleting the user. Please try again.',
+                        'error'
+                    );
+                }
+            });
+        } else {
+            console.log("Deletion cancelled by the user.");
+        }
+    });
+});
